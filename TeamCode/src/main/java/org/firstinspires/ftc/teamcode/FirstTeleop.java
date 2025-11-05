09c84869528961b225802dc4ae84a4bd5305e49d
jimmy1772421 <jimmy177242155672022@outlook.com>
2025-11-05 16:56:41 -0500

    first add

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleop.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class FirstTeleop extends OpMode {
+    @Override
+    public void init() {
+    //Happens once on init
+    }
+
+    @Override
+    public void init_loop() {
+        //Happens repeatedly during init
+        super.init_loop();
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        //Happens once after program starts
+    }
+
+    @Override
+    public void loop() {
+    //
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        //Happen after the program stops
+    }
+}
